89ce96626b229f8e3b092cfddebd436cc9fcea7b
Ethan Rowell <ethan.rowell11@gmail.com>
2016-07-28 17:39:54 -0700

    Added larger input, refactored comments and corrected output.

--- DiceCup/src/DiceCupMain.java
+++ DiceCup/src/DiceCupMain.java
@@ -15,39 +15,37 @@ public class DiceCupMain {
 			int dice_0 = s.nextInt();
 			int dice_1 = s.nextInt();
 	
-			int[] counts = new int[dice_0 + dice_1 +1];
+			int[] counts = new int[dice_0 + dice_1 + 1];
 			
+			// Count the occurrence of each sum of the dice.
 			for(int i = 1; i <= dice_0; i++) {
 				for(int j = 1; j <= dice_1; j++) {
 					int sum = i + j;
-					counts[sum] = counts[sum] + 1;
+					counts[sum]++;
 				}
 			}
 			
+			// the minimum dice roll sum is 2 (1 + 1)
 			int i = 2;
 			
-			//the sum of the dice roll
-			int sumOfRoll = i;
-			
-			//the count of the sum of the dice roll
+			// the count of the sum of the dice roll
 			int sumOfRollCount = counts[i];
 			
-			for(; i < counts.length -1; i++) {
-				if(counts[i+1] > counts[i]) {
-					sumOfRoll = i+1;
-					sumOfRollCount = counts[i+1];
+			// Find the highest occurrence of the sums.
+			for(; i < counts.length - 1; i++) {
+				if(counts[i + 1] > counts[i]) {
+					sumOfRollCount = counts[i + 1];
 				}
 			}
-			
-			System.out.println(sumOfRoll);
-			System.out.println();
 
+			// Print the sums that occurred the most
 			for(int j = 2; j < counts.length; j++) {
 				if(counts[j] == sumOfRollCount) {
 					System.out.println(j);
 				}
 			}
-			System.out.println();
+			
+			if(s.hasNextInt()) System.out.println();
 		}
 		s.close();
 	}
